1b6cfe53e5cb44777a7d0bb3c7dc656ba9fa01b7
MarcosGardinali <marcosgardinali21@outlook.com>
2023-10-22 10:26:27 -0300

    Analytics

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import Conteudo from "./Componentes/Conteudo";
+import { Analytics } from '@vercel/analytics/react';
 
 function App() {
   return (
     <div className="App">
       <Conteudo />
+      <Analytics />
     </div>
   );
 }
